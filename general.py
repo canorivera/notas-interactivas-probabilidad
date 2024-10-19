ce863c8926070e071e464d12de769ce5d74a2b4b
canorivera <cano_rivera@outlook.com>
2024-10-18 23:51:29 -0600

    Update general.py

--- general.py
+++ general.py
@@ -2,7 +2,10 @@ import streamlit as st
 st.header("Temario")
 
 st.markdown('''1. Fundamentos de probabilidad 
+            
             1. Variables aleatorias
+
             1. Distribuciones importantes
+            
             1. Distribuciones multivariadas
             ''')
