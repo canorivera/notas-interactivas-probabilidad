14edcf0b2ff8c1f565f14974b3f4fd56ec49080a
canorivera <cano_rivera@outlook.com>
2024-10-18 22:48:16 -0600

    Update notas.py

--- notas.py
+++ notas.py
@@ -2,9 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="Notas interactivas de probabilidad", page_icon="P.png")
 
-st.markdown("# Temario")
-st.markdown("1. Hola")
-
 fundamentos = st.Page("fundamentos.py", title="Fundamentos de probabilidad")
 vas = st.Page("vas.py", title="Variables aleatorias")
 distribuciones = st.Page("distribuciones.py", title="Distribuciones de probabilidad")
