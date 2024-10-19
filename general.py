7adc881b9d5abce9535c5612f79206ed61b2c44a
canorivera <cano_rivera@outlook.com>
2024-10-19 00:20:19 -0600

    Update general.py

--- general.py
+++ general.py
@@ -5,15 +5,72 @@ st.markdown('''
             1. Fundamentos de probabilidad 
 
                 1. Repaso
+
+                    1. Conjuntos
+                    2. Derivadas
+                    3. Series e integrales
+                    4. Matrices
                 2. Espacio muestral
-                3. Probabilidad clásica y frecuentista
-                4. Técnicas de conteo
-                5. Probabilidad condicional 
+                3. Función de probabilidad
+                4. Probabilidad clásica y frecuentista
+                5. Técnicas de conteo
+                6. Probabilidad condicional 
             
-                    1. Teorema de Bayes
-                    2. Independencia
-                    2. Ley de la probabilidad total 
+                    1. Independencia
+                    2. Teorema de Bayes
+                    2. Probabilidad total 
             2. Variables aleatorias
+                
+                1. Función de masa de probabilidad
+                2. Función de densidad de probabilidad
+                3. Función de distribución acumulada
+                4. Características numéricas de las variables aleatorias
+            
+                    1. Momentos y momentos centrales
+                    2. Tendencia central: media, mediana y moda
+                    3. Localizaciión: cuantiles
+                    4. Dispersión: varianza, desviación estándar, coeficiente de variación, amplitud intercuartil
+                    5. Simetría: coeficiente de asimetría, coeficiente de curtosis
+                5. Lotus o ley del estadístico inconsciente
+                6. Función generadora de momentos 
+                7. Desigualdades de Tchebysheb y Jensen
+                8. Transformaciones de vaariables aleatorias continuas
+            
+                    1. Método de la función de distribución acumulada
+                    2. Método de la transformación monótona
+                    3. Método de la función generadora de momentos
+
             3. Distribuciones importantes
+
+                1. Discretas
+
+                    1. Uniforme
+                    2. Bernouilli
+                    3. Binomial
+                    4. Binomial negativa
+                    5. Geométrica
+                    6. Hipergeométriica
+                    7. Poisson
+                2. Continuas
+                            
+                    1. Uniforme
+                    2. Gamma
+                    3. Exponencial
+                    4. $\chi^2$
+                    5. Normal  
+
             4. Distribuciones multivariadas
+
+                1. Función de densidad conjunta
+                2. Función de densidad marginal y función de densidad condicional 
+                3. Variables aleatorias independientes
+                4. Valor esperado y varianza de una función de variables aleatorias
+                5. Esperanza y varianza condicional
+                6. Momentos conjuntos de dos variables aleatorias: covarianza y coeficiente de correlación 
+                7. Función generadora de momentos conjunta
+                8. Distribución de una suma de variables aleatorias independientes
+                9. Distribución de una funcióno de variables aleatorias mediante el método de transformación
+                10. Estadísticos de orden: distribución del máximo y del mínimo
+                11. Distribución normal multivariada
+
             ''')
