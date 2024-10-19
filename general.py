9a0a0373f9c8156d4a1e08f5c2c8a59da73b5ff1
canorivera <cano_rivera@outlook.com>
2024-10-18 23:57:04 -0600

    Update general.py

--- general.py
+++ general.py
@@ -1,11 +1,19 @@
 import streamlit as st
 st.header("Temario")
 
-st.markdown('''1. Fundamentos de probabilidad 
-            
-            1. Variables aleatorias
+st.markdown('''
+            1. Fundamentos de probabilidad 
 
-            1. Distribuciones importantes
+                1. Repaso
+                2. Espacio muestral
+                3. Probabilidad clásica y frecuentista
+                4. Técnicas de conteo
+                5. Probabilidad condicional 
             
-            1. Distribuciones multivariadas
+                    1. Teorema de Bayes
+                    2. Independencia
+                    2. Ley de la probabilidad total 
+            2. Variables aleatorias
+            3. Distribuciones importantes
+            4. Distribuciones multivariadas
             ''')
