3636505108499700e3fcb51e23b392c9fb7d1441
canorivera <cano_rivera@outlook.com>
2024-10-19 01:56:19 -0600

    Update vas.py

--- vas.py
+++ vas.py
@@ -19,11 +19,11 @@ st.markdown(r'''
     2. $\intop_{-\infin}^{\infin} f_x(x) dx = 1$
             
     Además, cumple con las propiedades:
-    1. $P[a \leq x \leq b] = \intop_{a}^b f_x(x) dx$
+    - $P[a \leq x \leq b] = \intop_{a}^b f_x(x) dx$
     
-    2. $P[x = a] = \intop_{a}^a f_x(x) dx = 0$
+    - $P[x = a] = \intop_{a}^a f_x(x) dx = 0$
     
-    3. $P[x \leq a] = P[x < a]$
+    - $P[x \leq a] = P[x < a]$
 ''')
 
 
@@ -32,11 +32,11 @@ st.markdown(r'''
     Definición: se dice que la función $F_x(x): \Reals \to [0,1]$ es la función de distribución acumulada (f.d.a.) de la v.a. si $F_x(X) = P(X \leq x)$
             
     Además, si X es una v.a.c., entonces:
-    1. $P(a \leq x \leq b) = P(X \leq b) - P(X \leq a) = F_x(b) - F_x(a) $
+    - $P(a \leq x \leq b) = P(X \leq b) - P(X \leq a) = F_x(b) - F_x(a) $
     
-    2. $F_x(X) = P(X \leq x) = \int_{-\infin}^x f_x(t)dt$
+    - $F_x(X) = P(X \leq x) = \int_{-\infin}^x f_x(t)dt$
     
-    3. $f_x(x) = \frac{d}{dx}F_x(x) $
+    - $f_x(x) = \frac{d}{dx}F_x(x) $
     ''')
 
 st.markdown("## Características numéricas de las variables aleatorias")
@@ -64,7 +64,7 @@ st.markdown('''
 La ley del estadístico inconsciente nos permite calcular el valor esperado de la transformación de una variable aleatoria $g(X)$, de forma que:
 - Si $X$ es una v.a.d., $E[g(X)] = \sum_{x \in S_x} g(x) \cdot P(X = x)$
             
-- Si $X$ es una v.a.d., $E[g(X)] = \int_{x \in S_x} g(x) \cdot f_x(X) dx$
+- Si $X$ es una v.a.c., $E[g(X)] = \int_{x \in S_x} g(x) \cdot f_x(X) dx$
 ''')
 
 st.markdown(r''' 
@@ -81,5 +81,5 @@ Media: esperanza o primer momento
             
 Mediana: Si X es una v.a. con f.d.a. $F_x(X), entonces la mediana, $\tilde{x}$ es el valor del soporte de $X$ tal que:
 - Si X es discreta: $F_x(\tilde{x}^-) \leq \frac{1}{2} \leq F_x(\tilde{x})$
-- Si X es continua: $F_x(\tilde{x}) = \frac{1}{2}$
+- Si X es continua: $F_x(\tilde{x}) = P(X \leq \tilde{x}) = \frac{1}{2}$
             ''')
