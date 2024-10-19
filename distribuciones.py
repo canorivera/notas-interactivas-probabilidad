1ad6e6d9de0726e26cedc2d023433812555452a2
canorivera <cano_rivera@outlook.com>
2024-10-18 22:23:36 -0600

    Added several pages

---
+++ distribuciones.py
@@ -0,0 +1,2 @@
+import streamlit as st
+st.markdown("# Distribuciones de probabilidad importantes")
--- notas.py
+++ notas.py
@@ -1,3 +1,17 @@
 import streamlit as st
 
 st.header("Temario")
+
+fundamentos = st.Page("fundamentos.py", title="Fundamentos de probabilidad")
+vas = st.Page("vas.py", title="Variables aleatorias")
+distribuciones = st.Page("distribuciones.py", title="Distribuciones de probabilidad")
+multivariadas = st.Page("multivariadas.py", title = "Distribuciones multivariadas")
+
+pg = st.navigation(
+    {
+        "Primer parcial": [fundamentos],
+        "Segundo parcial": [vas, distribuciones],
+        "Tercer parcial": [multivariadas],
+    }
+)
+pg.run
