1dc5aa33b2e2249c94afa9897b37d1ae8eba871a
canorivera <cano_rivera@outlook.com>
2024-10-19 02:10:49 -0600

    distribuciones

--- distribuciones.py
+++ distribuciones.py
@@ -1,3 +1,53 @@
 import streamlit as st
-st.header("Distribuciones de probabilidad importantes")
-st.latex(r"u(x) = x^3")
+st.markdown("# Distribuciones de probabilidad importantes")
+st.markdown('''## Distribuciones discretas''')
+
+st.markdown('''
+### Uniforme
+            ''')
+
+st.markdown('''
+### Bernoulli
+            ''')
+
+st.markdown('''
+### Binomial
+            ''')
+
+st.markdown('''
+### Binomial negativa
+            ''')
+
+st.markdown('''
+### Geométrica
+            ''')
+
+st.markdown('''
+### Hipergeométrica
+            ''')
+
+st.markdown('''
+### Poisson
+            ''')
+
+st.markdown('''## Distribuciones continuas''')
+
+st.markdown('''
+### Uniforme
+            ''')
+
+st.markdown('''
+### Gamma
+            ''')
+
+st.markdown('''
+### Exponencial
+            ''')
+
+st.markdown('''
+### $\chi^2$
+            ''')
+
+st.markdown('''
+### Normal
+            ''')
