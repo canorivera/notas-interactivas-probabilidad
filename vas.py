f7dd123a6d8cfd3aea9be6b372ffa590c6dcbc9b
canorivera <cano_rivera@outlook.com>
2024-10-19 00:40:53 -0600

    Update vas.py

--- vas.py
+++ vas.py
@@ -1,2 +1,14 @@
 import streamlit as st
-st.header("Variables aleatorias")
+st.markdown("# Variables aleatorias")
+
+st.markdown('# Definición')
+st.markdown('Se dice que $X$ es una variable aleatoria si $:\omega \mapsto \Reals$. Es decir, las variables aleatorias son funciones reales definidas sobre un espacio muestral. La v.a. es discretas si solo puede tomar un número finito o contablemente infinito de valores. Por otro lado, la v.a. es continua si puede tomar infinitos valores en determinado intervalo.')
+
+st.markdown(r'''
+    ## Función de masa de probabilidad
+    Definición: Si $X$ es una v.a.d. con soporte $S_x = \{x_1, x_2, ... \}$ se dice que $f_x(x) = \Reals \to [0,1] es la función de masa de probabilidad (f.m.p.) si:$
+''')
+
+st.markdown("## Función de densidad de probabilidad")
+st.markdown("## Función de distribución acumulada")
+st.markdown("## Características numéricas de las variables aleatorias")
